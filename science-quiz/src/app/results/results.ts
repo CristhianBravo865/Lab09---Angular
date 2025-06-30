a8f539dc74586bc03ed40aefabfbe345537b852f
Cristhian <cristhian.bravo.8657@gmail.com>
2025-06-29 21:22:48 -0500

    Ejercicios Resueltos: Implementar ResultsComponent con su plantilla y estilos, y agregar ruta para resultados

--- science-quiz/src/app/results/results.ts
+++ science-quiz/src/app/results/results.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-results',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './results.html',
   styleUrl: './results.css'
 })
-export class Results {
+export class ResultsComponent {
+  constructor(private router: Router) {}
 
+  restartGame() {
+    this.router.navigate(['/']);
+  }
 }
